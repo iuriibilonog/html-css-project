5c552e4b149f7ebb00440d60b6964d0bac9adcde
Subm1s <82820375+Subm1s@users.noreply.github.com>
2021-09-01 16:50:03 +0300

    add slider

---
+++ js/slider/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.customer-reviews__slider').slick({
+        dots: true,
+        dotsClass:'customer-reviews__dots list',
+        arrows: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
