b3922d3dedf0aef97ce2743a16530d867f68d775
Lukavenko Sergej <myjavaname@gmail.com>
2021-09-01 13:09:31 +0300

    add modal forms

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,4 @@
-(() => {
+/* (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
@@ -9,4 +9,49 @@
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }
-})();
+}
+)();
+ */
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-product-1-open]'),
+    closeModalBtn: document.querySelector('[data-modal-product-1-close]'),
+    modal: document.querySelector('[data-modal-product-1]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+}
+)();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-product-2-open]'),
+    closeModalBtn: document.querySelector('[data-modal-product-2-close]'),
+    modal: document.querySelector('[data-modal-product-2]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+}
+
+)();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-product-3-open]'),
+    closeModalBtn: document.querySelector('[data-modal-product-3-close]'),
+    modal: document.querySelector('[data-modal-product-3]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+}
+
+)();
