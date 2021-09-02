402f88d451f6150dfeaf6c116b571389bef2163f
Iurii Bilonog <iuriibilonog@gmail.com>
2021-09-02 18:34:57 +0300

    fixed bugs in modal hero

--- js/modal.js
+++ js/modal.js
@@ -158,16 +158,16 @@
 
 // MODAL TEAM
 
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-modal-team-open]'),
-//     closeModalBtn: document.querySelector('[data-modal-team-close]'),
-//     modal: document.querySelector('[data-modal-team]'),
-//   };
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-//   function toggleModal() {
-//     refs.modal.classList.toggle('is-hidden');
-//   }
-// }
-// )();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-team-open]'),
+    closeModalBtn: document.querySelector('[data-modal-team-close]'),
+    modal: document.querySelector('[data-modal-team]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+}
+)();
