810a902cf6b9a5e6af9aecb1144c33f834b4ec41
Iurii Bilonog <iuriibilonog@gmail.com>
2021-09-03 00:42:24 +0300

    add scroll-up button

--- js/scroll.js
+++ js/scroll.js
@@ -17,4 +17,28 @@ $(document).ready(function(){
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
     });
- });
+});
+ 
+// SCROLL UP BUTTON
+
+$(function() {
+  // при нажатии на кнопку scrollup
+  $('.scroll-up').click(function() {
+    // переместиться в верхнюю часть страницы
+    $("html, body").animate({
+      scrollTop:0
+    },1000);
+  })
+})
+// при прокрутке окна (window)
+$(window).scroll(function() {
+  // если пользователь прокрутил страницу более чем на 200px
+  if ($(this).scrollTop()>200) {
+    // то сделать кнопку scrollup видимой
+    $('.scroll-up').fadeIn();
+  }
+  // иначе скрыть кнопку scrollup
+  else {
+    $('.scroll-up').fadeOut();
+  }
+});
