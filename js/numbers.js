39b3e228a81a2683fc63a14a298f390e20e39983
Iurii Bilonog <iuriibilonog@gmail.com>
2021-09-02 13:54:42 +0300

    fixed new bugs in time count

--- js/numbers.js
+++ js/numbers.js
@@ -14,32 +14,22 @@ $('.representations-number').each(function () {
 
 // TIME COUNT IN ABOUT 
 
-$('.about__point').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: Number($(this).attr("data-duration")),
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
+var number = document.querySelector('.about__point'),
+		numberTop = number.getBoundingClientRect().top,
+    start = +number.innerHTML, end = +number.dataset.max;
+
+window.addEventListener('scroll', function onScroll() {
+		if(window.pageYOffset > numberTop - window.innerHeight / 2) {
+    		this.removeEventListener('scroll', onScroll);
+        var interval = setInterval(function() {
+        		number.innerHTML = ++start;
+            if(start == end) {
+            		clearInterval(interval);
+            }
+        }, 5);
+    }
 });
 
-// var number = document.querySelector('.about__point'),
-// 		numberTop = number.getBoundingClientRect().top,
-//     start = +number.innerHTML, end = +number.dataset.max;
 
-// window.addEventListener('scroll', function onScroll() {
-// 		if(window.pageYOffset > numberTop - window.innerHeight / 2) {
-//     		this.removeEventListener('scroll', onScroll);
-//         var interval = setInterval(function() {
-//         		number.innerHTML = ++start;
-//             if(start == end) {
-//             		clearInterval(interval);
-//             }
-//         }, 5);
-//     }
-// });
 
 
