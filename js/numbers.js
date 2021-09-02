4e9ed97d5614d139dec813e7be482657a91b8794
Iurii Bilonog <iuriibilonog@gmail.com>
2021-09-02 13:35:42 +0300

    fixed time count animation

--- js/numbers.js
+++ js/numbers.js
@@ -26,4 +26,20 @@ $('.about__point').each(function () {
     });
 });
 
+// var number = document.querySelector('.about__point'),
+// 		numberTop = number.getBoundingClientRect().top,
+//     start = +number.innerHTML, end = +number.dataset.max;
+
+// window.addEventListener('scroll', function onScroll() {
+// 		if(window.pageYOffset > numberTop - window.innerHeight / 2) {
+//     		this.removeEventListener('scroll', onScroll);
+//         var interval = setInterval(function() {
+//         		number.innerHTML = ++start;
+//             if(start == end) {
+//             		clearInterval(interval);
+//             }
+//         }, 5);
+//     }
+// });
+
 
