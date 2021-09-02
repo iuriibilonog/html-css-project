124a3299a3b5355640c6f71e1df22f8f5c8df616
Iurii Bilonog <iuriibilonog@gmail.com>
2021-09-03 00:25:23 +0300

    добавил якоря

---
+++ js/scroll.js
@@ -0,0 +1,20 @@
+// HERO BUTTONS
+ $(document).ready(function(){
+    $("#hero-buttons").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+ });
+
+// HEADER LINKS
+
+$(document).ready(function(){
+    $("#header-links").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+ });
